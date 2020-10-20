0c456dca33092754d4e204015bd51b5aba55f25b
AnshulToshniwal <58212114+AnshulToshniwal@users.noreply.github.com>
2020-10-20 17:54:57 +0200

    Add files via upload

--- projects/project1/scripts/ridge_regression.py
+++ projects/project1/scripts/ridge_regression.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Exercise 3.
+
 
 Ridge Regression
 """
@@ -8,12 +8,11 @@ import numpy as np
 
 
 def ridge_regression(y, tx, lambda_):
-    """implement ridge regression."""
-    # ***************************************************
-    w =  np.dot(np.linalg.inv(np.dot(tx.T,tx)+ lambda_*np.identity(np.shape(tx)[1])),np.dot(tx.T,y))
+    # The optimum w
+    w =  np.dot(np.linalg.inv(np.dot(tx.T,tx)+ (lambda_*2*np.shape(y)[0])*np.identity(np.shape(tx)[1])),np.dot(tx.T,y))
     # loss
     e = y - tx.dot(w)
     mse = e.dot(e) / (2 * len(e)) + lambda_*(np.inner(w,w))
-    # ***************************************************
+
     
     return w,mse 
