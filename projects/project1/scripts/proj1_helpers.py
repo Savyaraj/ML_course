567bfc6433142ea1cbb8885cd0e3a744ef44a42b
KseniiaSShevchenko <72921984+KseniiaSShevchenko@users.noreply.github.com>
2020-10-21 13:17:40 +0200

    Correction functions load_csv_data, predict_labels and addition functions standardize, correction_missing_values

--- projects/project1/scripts/proj1_helpers.py
+++ projects/project1/scripts/proj1_helpers.py
@@ -11,9 +11,9 @@ def load_csv_data(data_path, sub_sample=False):
     ids = x[:, 0].astype(np.int)
     input_data = x[:, 2:]
 
-    # convert class labels from strings to binary (-1,1)
+    # convert class labels from strings to binary (0,1)
     yb = np.ones(len(y))
-    yb[np.where(y=='b')] = -1
+    yb[np.where(y=='b')] = 0
     
     # sub-sample
     if sub_sample:
@@ -27,8 +27,8 @@ def load_csv_data(data_path, sub_sample=False):
 def predict_labels(weights, data):
     """Generates class predictions given weights, and a test data matrix"""
     y_pred = np.dot(data, weights)
-    y_pred[np.where(y_pred <= 0)] = -1
-    y_pred[np.where(y_pred > 0)] = 1
+    y_pred[np.where(y_pred <= 0.5)] = 0
+    y_pred[np.where(y_pred > 0.5)] = 1
     
     return y_pred
 
@@ -46,3 +46,24 @@ def create_csv_submission(ids, y_pred, name):
         writer.writeheader()
         for r1, r2 in zip(ids, y_pred):
             writer.writerow({'Id':int(r1),'Prediction':int(r2)})
+            
+def standardize(x):
+    """Standardize the original data set."""
+    mean_x = np.mean(x, axis = 0)
+    x = x - mean_x
+    std_x = np.std(x, axis = 0)
+    x = x / std_x
+    return x, mean_x, std_x
+
+def correction_missing_values(tX):
+    """Correction missing values by mean value of feature"""
+    for j in range (tX.shape[1]):
+        tX_val = []
+        for i in range (tX.shape[0]):
+            if (tX[i, j] != -999):
+                tX_val.append(tX[i,j])
+        mean_tX_val = np.mean(tX_val)
+        for i in range (tX.shape[0]):
+            if (tX[i, j] == -999):
+                tX[i,j] = mean_tX_val
+    return tX
